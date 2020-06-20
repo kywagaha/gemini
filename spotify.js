4bdba96ce1f79e7f0861038162f8d10a1806d8b3
kyleawayan <kyle@awayan.com>
2020-06-19 23:05:06 -0700

    connect spotify api to webpage

--- spotify.js
+++ spotify.js
@@ -1,10 +1,13 @@
 var SpotifyWebApi = require('spotify-web-api-js');
 var spotifyApi = new SpotifyWebApi();
 
-spotifyApi.setAccessToken('spotifytoken');
+spotifyApi.setAccessToken('token');
 spotifyApi.getMyCurrentPlayingTrack('title')
 .then(function(data) {
-    console.log('User playlists', data);
-  }, function(err) {
-    console.error(err);
-  });
+  console.log('Now playing', data);
+  document.getElementById("song").innerHTML = data.item.name
+  document.getElementById("artist").innerHTML = data.item.artists[0].name
+  document.body.style.backgroundImage = 'url('+data.item.album.images[0].url+')'
+}, function(err) {
+  console.error(err);
+});
