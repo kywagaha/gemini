69cdfabad870f5b5f10a0fe7ee3b5b7088da22b0
Kyle Awayan <kyle@awayan.com>
2020-08-19 16:43:16 -0700

    fix placeholders

--- spotify.js
+++ spotify.js
@@ -5,8 +5,8 @@ const path = './file.txt';
 var spotifyApi = new SpotifyWebApi();
 
 // Set Spotify app client variables here:
-const CLIENT_ID = 'h';
-const CLIENT_SECRET = 'h';
+const CLIENT_ID = 'CLIENT_ID';
+const CLIENT_SECRET = 'CLIENT_SECRET';
 
 const update_ms = 5000;
 const URI = 'http://localhost:8080/callback';
