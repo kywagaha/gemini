8298487489fafb4baf5945eebaf90d57d6063b68
Gabe-H <gabe.haarberg@gmail.com>
2020-08-24 23:24:17 -0700

    Version 3, all tokens handled by node

--- spotify.js
+++ spotify.js
@@ -1,132 +1,120 @@
-var SpotifyWebApi = require('spotify-web-api-js');
 var $ = require('jquery');
-var spotifyApi = new SpotifyWebApi();
 
-function enable() {
+function control(type){
     $.ajax({
-        async: false,
-        url: 'http://localhost:8888/auth/enable',
+        async: true,
+        url: 'http://localhost:8080/control',
+        data: {
+            type: type
+        },
         type: 'GET'
     });
 };
-enable();
-$.ajax({
-    async: false,
-    url: 'http://localhost:8888/auth/tokens',
-    type: 'GET',
-    success: function(data) {
-        spotifyApi.setAccessToken(data.access_token);
-        getOnce();
-        setInterval(refresh, ((data.expires_in_sec - 10) * 1000))
-    }
-});
 
-function refresh() {
-    enable();
+function start(){
     $.ajax({
-        async: true,
-        url: 'http://localhost:8888/auth/refresh',
+        async: false,
+        url: 'http://localhost:8080/currently-playing',
         type: 'GET',
         success: function(data) {
-            spotifyApi.setAccessToken(data.body.access_token);
+            if (data.statusCode == 200){
+                document.getElementById("song").innerHTML = data.body.item.name;
+                document.getElementById("artist").innerHTML = data.body.item.artists[0].name;
+                document.body.style.backgroundImage = 'url('+data.body.item.album.images[0].url+')';
+                setInterval(update, 5000);
+            }
+            else {
+                document.getElementById("song").innerHTML = 'No track loaded';
+                document.getElementById("artist").innerHTML = 'please play a track';
+                document.body.style.backgroundImage = '';
+                mySong = null;
+                setInterval(update, 5000);
+            };
         }
     });
 };
-
-function getOnce(){
-    spotifyApi.getMyCurrentPlayingTrack().then(function(data) {
-        if (data != ""){
-            document.getElementById("song").innerHTML = data.item.name;
-            document.getElementById("artist").innerHTML = data.item.artists[0].name;
-            document.body.style.backgroundImage = 'url('+data.item.album.images[0].url+')';
-            setInterval(update, 5000);
-        }
-        else {
-            document.getElementById("song").innerHTML = 'No track loaded';
-            document.getElementById("artist").innerHTML = 'please play a track';
-            document.body.style.backgroundImage = '';
-            mySong = null;
-            setInterval(update, 5000);
-        };
-    });
-};
+start();
 
 var mySong;
 function update(){
-    spotifyApi.getMyCurrentPlayingTrack().then(function(data) {
-        if (data != ""){
-            if (mySong != data.item.name) {
-                document.getElementById("song").innerHTML = data.item.name;
-                document.getElementById("artist").innerHTML = data.item.artists[0].name;
-                document.body.style.backgroundImage = 'url('+data.item.album.images[0].url+')';
+    $.ajax({
+        async: true,
+        url: 'http://localhost:8080/currently-playing',
+        type: 'GET',
+        success: function(data) {
+            if (data.statusCode == 200){
+                if (mySong != data.body.item.name) {
+                    document.getElementById("song").innerHTML = data.body.item.name;
+                    document.getElementById("artist").innerHTML = data.body.item.artists[0].name;
+                    document.body.style.backgroundImage = 'url('+data.body.item.album.images[0].url+')';
+                };
+                var remaining_ms = data.body.item.duration_ms - data.body.progress_ms;
+                if (remaining_ms < 5000) {
+                    setTimeout(update, remaining_ms);
+                    console.log('Predicting track skip in ' + remaining_ms);
+                };
+                mySong = data.body.item.name;
+            }
+            else if (data.statusCode == 204) {
+                document.getElementById("song").innerHTML = 'No track loaded';
+                document.getElementById("artist").innerHTML = 'please play a track';
+                console.log('No loaded track found');
+                mySong = null;
+            }
+            else {
+                document.getElementById("song").innerHTML = 'Error';
+                document.getElementById("artist").innerHTML = 'check console logs';
+                console.log(data);
             };
-            var remaining_ms = data.item.duration_ms - data.progress_ms;
-            if (remaining_ms < 5000) {
-                setTimeout(update, remaining_ms);
-                console.log('Predicting track skip in ' + remaining_ms);
-            };
-            mySong = data.item.name;
         }
-        else {
-        
-            document.getElementById("song").innerHTML = 'No track loaded';
-            document.getElementById("artist").innerHTML = 'please play a track';
-            console.log('No loaded track found');
-            mySong = null;
-        };
-        },function(err) {
-            console.log(err.status);
-            // Handle bad API
-            if (err.status == 401) {
-                document.getElementById("song").innerHTML = "API token out of date";
-                document.getElementById("artist").innerHTML = "please close and restart the app";
-            };
-        }); 
-
-};
-
-// Get mouse x position
-function xCoords(event) { 
-    var x = event.clientX;
-    return x;
-};
-
-
-// Toggle playback
-var singleClick = function(){
-    spotifyApi.getMyCurrentPlayingTrack().then(function(data) {
-        var isPlaying = data.is_playing;
-        if (isPlaying == true){
-            spotifyApi.pause();
-            console.log('Pausing music');
-        }
-        else if (isPlaying == false){
-            spotifyApi.play();
-            console.log('Playing music');
-        }
-        else {
-            console.log('No track loaded');
-        };
     });
 };
 
+// Toggle playback
+var singleClick = function(){
+    $.ajax({
+        async: true,
+        url: 'http://localhost:8080/currently-playing',
+        type: 'GET',
+        success: function(data) {
+            console.log(data.body);
+            var isPlaying = data.body.is_playing;
+            // Play if paused; pause if playing
+            if (isPlaying == true){
+                control('pause');
+                console.log('Pausing music');
+            }
+            else if (isPlaying == false){
+                control('play');
+                console.log('Playing music');
+            }
+            else {
+                console.log('No track loaded');
+            };
+        }
+    });
+};
+
+// Get mouse x position
+function xCoords(event) {
+    var x = event.clientX;
+    return x;
+};
 // skip to next song in queue
 var doubleClickFwd = function(){
     console.log('Skipping forward');
-    spotifyApi.skipToNext();
+    control('forward');
     setTimeout(update, 400);
 };
 // skip back to previous song
 var doubleClickBkwd = function(){
     console.log('Skipping backward');
     setTimeout(update, 400); 
-    spotifyApi.skipToPrevious();
+    control('backward');
 };
-
+// Single click function
 var firing = false;
 var firingFunc = singleClick;
-
-// Single click function
 window.onclick = function() {
     if(firing)
         return;
