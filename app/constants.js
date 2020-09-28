b5d0a852dfc8b599b94bfd8a4835358c885c4caf
Gabe-H <gabe.haarberg@gmail.com>
2020-09-27 18:43:37 -0700

    file rename

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,7 @@
 const SpotifyWebApi = require("spotify-web-api-node");
 const spotifyApi = new SpotifyWebApi();
 const settings = require("electron-settings");
+settings.configure({ fileName: 'Settings' })
 
 module.exports = {
   CLIENT_ID : 'cfa0ee03cb904889af4c77929c6d2ba8',
